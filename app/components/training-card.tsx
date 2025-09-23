d232958b5cff51ba83670c3aa113583f4451c2ed
Jakob Kunze <jakob.n.kunze@gmail.com>
2025-09-23 23:28:18 +0200

    fix: change width of collapsed cards

--- app/components/training-card.tsx
+++ app/components/training-card.tsx
@@ -15,7 +15,7 @@ export function TrainingCard({content, collapsed}: {content:string, collapsed: b
         </CardHeader>
         <CardContent>
           {collapsed 
-            ? `${content?.slice(0, 12) || ""}...`
+            ? `${content?.slice(0, 18) || ""}...`
             : content ?? "Daten nicht geladen"
           }
         </CardContent>
